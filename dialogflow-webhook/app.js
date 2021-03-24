7078ff9b40d1ad6730590d18f4af9d65aaafc3b3
kelvin-22 <kelvin.raharja@22squared.com>
2021-03-24 09:28:24 -0400

    rearchitectured webhook with answer and question handlers

--- dialogflow-webhook/app.js
+++ dialogflow-webhook/app.js
@@ -23,64 +23,113 @@ router.post('/', (request, response) => {
     );
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
+    // Variable Declerations
+    var myQuestion = '';
+    var myResponseType = '';
+    let questionObj = {
+        question1: {
+            question: `How would you rate your current stress level using a 1-5 scale (1 = not stressed at all and 5 = extremely stressed)`,
+            expectedResponseType: `ratingResponse`,
+        },
+        question2: {
+            question: `How stressed are you about school? 1 = not stressed at all and 5 = extremely stressed`,
+            expectedResponseType: `ratingResponse`,
+        },
+        question3: {
+            question: `How stressed are you about the upcoming game? 1 = not stressed at all and 5 = extremely stressed)`,
+            expectedResponseType: `ratingResponse`,
+        },
+        question4: {
+            question: `What is one thing you'd like for your coach to know about your stress level right now?`,
+            expectedResponseType: `openendedResponse`,
+        },
+        question5: {
+            question: `Pick any emoji that best represents how you are feeling at this moment.`,
+            expectedResponseType: `emojiResponse`,
+        },
+    };
+
+    // General Notifications
     function welcome(agent) {
+        myQuestion = questionObj.question1.question;
+        myResponseType = questionObj.question1.expectedResponseType;
+        console.log('question: ' + myQuestion);
+        agent.setContext({
+            name: myResponseType,
+            lifespan: 1,
+            parameters: { currentQuestion: 1 },
+        });
         agent.add(
-            `Welcome to the athlete survey. Here comes your first question. `
+            `Welcome to the athlete survey. Here comes your first question. ` +
+                myQuestion
         );
     }
 
     function fallback(agent) {
         agent.add(`I didn't understand`);
         agent.add(`I'm sorry, can you try again?`);
+        agent.add(`Sorry, I didn't catch that.`);
     }
 
-    function thankyou(agent) {
-        agent.add(`Thank you for your time. Have a nice day.`);
+    // Question Handlers
+    // function askQuestionHandler(agent, questionNumber) {
+    //     var contexts = JSON.stringify(
+    //         request.body.queryResult.outputContexts[0].parameters
+    //             .currentQuestion
+    //     );
+    //     var questionOneResponse = JSON.stringify(
+    //         request.body.queryResult.outputContexts[0].parameters
+    //             .question1Response
+    //     );
+    //     console.log('question number: ' + questionNumber);
+    //     agent.add(`Asking question number ` + questionNumber);
+    // }
+
+    // Response Handlers
+
+    function emojiHandler(agent) {
+        agent.add("I see you're using emoji response");
     }
 
-    function endSession(agent) {
-        agent.add(`Alright, maybe next time. Have a great day!`);
+    function yesnoHandler(agent) {
+        agent.add('You answered ' + userChoice + '.');
     }
 
-    function questionOne(agent) {
-        var contexts = JSON.stringify(
+    function ratingHandler(agent) {
+        var myRatingResponse = JSON.stringify(
             request.body.queryResult.outputContexts[0].parameters
-                .currentQuestion
+                .myRatingResponse
         );
-        console.log('contexts: ' + contexts);
-        agent.add(
-            `On a scale of 1 to 10 (from worst to best), how are you feeling today?`
-        );
-    }
-
-    function questionTwo(agent) {
-        var contexts = JSON.stringify(
-            request.body.queryResult.outputContexts[0].parameters
-                .currentQuestion
-        );
-        var questionOneResponse = JSON.stringify(
-            request.body.queryResult.outputContexts[0].parameters
-                .question1Response
-        );
-        console.log('contexts: ' + contexts);
-        if (
-            Number(questionOneResponse) > 0 &&
-            Number(questionOneResponse) < 11
-        ) {
-            agent.add(`What is your general mood after the game?`);
+        console.log('my rating: ' + myRatingResponse);
+        if (myRatingResponse < 1 && myRatingResponse > 5) {
+            agent.add(
+                `You entered ` +
+                    myRatingResponse +
+                    `. Please enter a number between 1 and 5.`
+            );
         } else {
-            agent.add(`Please enter a number between 1 and 10.`);
+            agent.add(`You entered ` + myRatingResponse);
         }
+    }
+
+    function openEndedHandler(agent) {
+        agent.add(`You answered with an open ended response.`);
     }
 
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
     intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('Question 1', questionOne);
-    intentMap.set('Decline Survey', endSession);
-    intentMap.set('Question 2', questionTwo);
-    intentMap.set('End Survey', thankyou);
+    intentMap.set('Emoji handling - Activity', emojiHandler);
+    intentMap.set('Emoji handling - Animals & Nature', emojiHandler);
+    intentMap.set('Emoji handling - Food & Drink', emojiHandler);
+    intentMap.set('Emoji handling - Objects', emojiHandler);
+    intentMap.set('Emoji handling - Smileys & People', emojiHandler);
+    intentMap.set('Emoji handling - Symbols', emojiHandler);
+    intentMap.set('Emoji handling - Travel & Places', emojiHandler);
+    intentMap.set('Response handling - yesno', yesnoHandler);
+    intentMap.set('Response handling - rating', ratingHandler);
+    intentMap.set('Response handling - open ended', openEndedHandler);
     agent.handleRequest(intentMap);
 });
 
